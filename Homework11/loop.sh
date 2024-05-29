fdeeeb433947d3626d7e63dbf37c8c227a6caea9
MOHKIRA111 <m.eshimova111@gmail.com>
2024-05-29 18:34:57 +0000

    modified

--- Homework11/loop.sh
+++ Homework11/loop.sh
@@ -21,6 +21,15 @@ function user() {
    for b in httpd  ghostscript mysql-server php php-bcmath php-curl php-imagick php-intl php-json php-mbstring php-mysql php-xml php-zip
    do 
   yum install -y $b
+   wget https://wordpress.org/latest.tar.gz
+   tar zxf latest.tar.gz
+    mv wordpress /var/www/html/
+    chown -R www-data:www-data /var/www/html/
+    chmod 755 /var/www/html/
+   systemctl start hhtpd
+   systemctl enable httpd
+   systemctl restart httpd
+   
 done
 
 } 
@@ -37,6 +46,3 @@ elif [[ $function_name == "install" ]]; then
 else
     echo "Invalid function name. Please enter either 'user' 'folder' or 'install'."
 fi
-
-
-
